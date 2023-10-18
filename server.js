a8de603993568d9c63b2d585af6349d88c2f28a1
siddhima22 <114576057+siddhima22@users.noreply.github.com>
2023-10-18 16:02:20 +0530

    new version no.2

--- server.js
+++ server.js
@@ -8,9 +8,19 @@ app.set('view engine', 'ejs'); // Set EJS as the template engine
 const cart = [];
 const wishlist = [];
 
+app.use(express.json()); 
 app.get('/', (req, res) => {
-  // Render the index.ejs file when accessing the root URL
-  res.render('index', { pageTitle: 'Product Description' });
+    const product = "Nike Men's Grey Jersey"; // Replace this with the actual product data
+    res.render('index', { pageTitle: 'Product Description', product: product });
+});
+app.get('/protein', (req, res) => {
+    const product = "Bigmuscles Protein Powder"; // Replace this with the actual product data
+    res.render('protein', { pageTitle: 'Product Description', product: product });
+});
+
+app.get('/dumbell', (req, res) => {
+    const product = "Dumbell"; // Replace this with the actual product data
+    res.render('dumbell', { pageTitle: 'Product Description', product: product });
 });
 
 app.get('/cart', (req, res) => {
@@ -23,48 +33,33 @@ app.get('/wish', (req, res) => {
   res.render('wishlist', { pageTitle: 'My Wishlist', wishlist });
 });
 
-// Add item to cart
 app.get('/addToCart', (req, res) => {
-  const product = {
-    name: 'Nike Men\'s Grey Jersey', // Product name
-    quantity: 1, // Initial quantity
-  };
+    const product = {
+        name: 'Nike Men\'s Grey Jersey', // Product name
+        quantity: 1, // Initial quantity
+    };
 
-  const itemIndex = cart.findIndex((item) => item.name === product.name);
+    // Check if the product is already in the cart
+    const itemIndex = cart.findIndex(item => item.name === product.name);
 
-  if (itemIndex === -1) {
-    // Item not in cart, add it
-    cart.push(product);
-  } else {
-    // Item already in cart, increment quantity (limited to 5)
-    if (cart[itemIndex].quantity < 5) {
-      cart[itemIndex].quantity++;
+    if (itemIndex === -1) {
+        // Item not in cart, add it
+        cart.push(product);
     } else {
-      // Limit reached
-      res.send('Sorry, this item is out of stock.');
-      return;
+        // Item already in cart, increment quantity (limited to 5)
+        if (cart[itemIndex].quantity < 5) {
+            cart[itemIndex].quantity++;
+        } else {
+            // Limit reached
+            res.status(400).json({ message: 'Sorry, this item is out of stock.' });
+            return;
+        }
     }
-  }
 
-  // Redirect back to the product description page
-  res.redirect('/');
+    // Send a success response with a message
+    res.json({ message: 'Product added to cart.' });
 });
 
-// Add item to wishlist
-app.get('/addToWishlist', (req, res) => {
-  const product = {
-    name: 'Nike Men\'s Grey Jersey', // Product name
-  };
-
-  // Check if the product is already in the wishlist
-  if (!wishlist.some((item) => item.name === product.name)) {
-    // Add the product to the wishlist
-    wishlist.push(product);
-  }
-
-  // Redirect back to the product description page
-  res.redirect('/');
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
